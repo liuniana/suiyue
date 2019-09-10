cae386992e6ada7ec6729e8e5cceb22748d8bb70
liuniana <121709293@qq.com>
2019-09-10 23:31:28 +0800

    测试

---
+++ 后羿/houyi.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<Windows.h>
+#include<string>
+
+using namespace std;
+
+//后羿射日
+/*int main(void){
+	for(int i=1;i<=9;i++){
+		cout<<"射第"<<i<<"个太阳"<<endl;
+	}
+	system("pause");
+	return 0;*/
+int main(void){
+	for(int i=1;i<=9;i++){
+		cout<<"射第"<<i<<"个太阳"<<endl;
+	}
+	system("pause");
+	return 0;
+}
